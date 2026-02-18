d41cf83d89d6db9737d3b927bc135e3c344842fe
harpreet kaur gothra <harpreetk2146@gmail.com>
2026-02-19 02:15:10 +0530

    Add export controller load test script

---
+++ backend/test-export.js
@@ -0,0 +1,9 @@
+﻿console.log('Testing export controller...');
+try {
+  const exportController = require('./src/controllers/export.controller');
+  console.log('✓ Export controller loaded');
+  console.log('  Methods:', Object.keys(exportController));
+} catch (e) {
+  console.error('✗ Export controller FAILED:', e.message);
+  console.error('  Stack:', e.stack);
+}
